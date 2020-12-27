239a12fd6977ff000c65b7264d034d4fc3a132cc
Landon Lloyd <65516863+LandonLloyd@users.noreply.github.com>
2020-12-26 20:01:10 -0700

    Inputting Teams Make the functionality for inputting team names. Next up is matchmaking!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-function matches() {
-	console.log("it works!");
+let button = document.getElementById("addTeams");
+let entered = document.getElementById("teamsEntered");
+
+let input = document.getElementById("add");
+let teams = [];
+
+document.addEventListener("keydown", (event) => {
+	if (event.key === "Enter") {
+		addTeamNames();
+	}
+});
+button.addEventListener("click", addTeamNames);
+function addTeamNames() {
+	if (input.value === "") {
+		return;
+	}
+	teams.push(input.value);
+	entered.innerHTML = `Teams Entered: ${teams}`;
+	input.value = "";
+
+	console.log(teams);
 }
