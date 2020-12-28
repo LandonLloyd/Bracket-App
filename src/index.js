9759e8cc9984a581778cf94a19c66ad2655294c1
Landon Lloyd <65516863+LandonLloyd@users.noreply.github.com>
2020-12-28 15:35:00 -0700

    Small Matchmaking change

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 //THIS IS FOR ENTERING TEAMS
 
 let button = document.getElementById("addTeams");
+
 let entered = document.getElementById("teamsEntered");
+
 let clearTeams = document.getElementById("clearTeams");
 
 let input = document.getElementById("add");
+
 let teams = [];
 
 clearTeams.addEventListener("click", function () {
 	teams = [];
+	console.log(teams);
 	entered.innerHTML = "Teams Entered: ";
 	matchesText.innerHTML = "";
 });
@@ -18,30 +22,39 @@ document.addEventListener("keydown", (event) => {
 		addTeamNames();
 	}
 });
+
 button.addEventListener("click", addTeamNames);
 function addTeamNames() {
 	if (input.value === "") {
 		return;
 	}
 	teams.push(input.value);
+	console.log(teams);
 	entered.innerHTML = `Teams Entered: ${teams}`;
 	input.value = "";
 }
 //THIS IS FOR CREATING MATCHMAKING
 
 let matchmaker = document.getElementById("createMatchup");
-let counter = teams.length / 2;
 
 matchmaker.addEventListener("click", matchmaking);
 
 let matchesText = document.getElementById("matches");
+
 function matchmaking() {
 	let random = Math.floor(Math.random() * teams.length);
 	let random2 = Math.floor(Math.random() * teams.length);
+	if (random === random2) {
+		random = Math.floor(Math.random() * teams.length);
+	}
 
 	let match = document.createTextNode(
 		`${teams[random]} VS. ${teams[random2]}`
 	);
+
+	teams.splice(random, 1);
+	teams.splice(random2, 1);
+	console.log(teams);
 	let lineBreak = document.createElement("br");
 	let winnerButton = document.createElement("button");
 	let randButton = document.createElement("button");
